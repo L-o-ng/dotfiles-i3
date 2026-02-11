b531569b7ed2896b145b06ac37239dfa11086629
Dylan Thompson <dylant7192@gmail.com>
2026-02-11 14:19:33 +0000

    update nvim with LSP

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,19 +2,35 @@
 require("config.lazy")
 vim.cmd.colorscheme "catppuccin-mocha"
 
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
-vim.cmd("set clipboard+=unnamedplus")
-vim.cmd("set foldlevelstart=50")
-vim.cmd("set number")
-vim.cmd("set relativenumber")
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.clipboard:append("unnamedplus")
+vim.opt.foldlevelstart = 50
+vim.opt.number = true
+vim.opt.relativenumber = true
 
 local telescope_builtin = require("telescope.builtin")
 local treesitter_builtin = require("nvim-treesitter")
 
-require('lualine').setup()
+require("lualine").setup()
+require("mason").setup()
+require("mason-lspconfig").setup({
+  ensure_installed = { "lua_ls" }
+})
+require("telescope").setup {
+  extensions = {
+    ["ui-select"] = {
+      require("telescope.themes").get_dropdown {}
+    }
+  }
+}
+require("telescope").load_extension("ui-select")
+
+vim.diagnostic.config({
+  virtual_text = true
+})
 treesitter_builtin.setup { install_dir = vim.fn.stdpath('data') .. '/site' }
 treesitter_builtin.install = { "lua", "rust", "c" }
 vim.api.nvim_create_autocmd('FileType', {
@@ -26,4 +42,7 @@ vim.wo[0][0].foldmethod = 'expr'
 
 vim.keymap.set('n', '<C-p>', telescope_builtin.find_files, {})
 vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal right<CR>', {})
+vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+vim.keymap.set({'n', 'v'}, '<space>ca', vim.lsp.buf.code_action, {})
 
---
+++ nvim/lua/plugins/lspconf.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "mason-org/mason.nvim",
+    opts = {}
+  },
+  {
+    "mason-org/mason-lspconfig.nvim",
+    opts = {},
+  },
+  {
+    "neovim/nvim-lspconfig",
+  },
+  {
+    "nvim-telescope/telescope-ui-select.nvim",
+  }
+}
+
